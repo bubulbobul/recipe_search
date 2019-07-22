4aad7c387c8560a9f1efa10b6c3a1c53e8a43b56
windam <windam@live>
2019-07-22 16:33:28 +0530

    getRecipe fct

---
+++ src/components/Form.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Form(props) {
+  return (
+    <form onSubmit={props.getRecipe}>
+      <input type='text' />
+      <button>Search</button>
+    </form>
+  );
+}
