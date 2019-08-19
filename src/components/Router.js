b2061e390a7a37ff53139e66179e253e7bef7ee3
bubulbobul <windam@live.fr>
2019-08-19 15:52:17 +0530

    ras

--- src/components/Router.js
+++ src/components/Router.js
@@ -11,11 +11,24 @@ const Router = () => {
   return (
     <BrowserRouter basename={`${process.env.PUBLIC_URL}`}>
       <Switch>
-        <Route exact path={`${process.env.PUBLIC_URL}/`} component={App} />
+        {/* <Route exact path={`/`} component={App} /> */}
+        {/* <Route exact path={`${process.env.PUBLIC_URL}/`} component={App} /> */}
         <Route
+          exact
+          path={`/`}
+          render={routerProps => <App routerProps={routerProps} />}
+        />
+
+        <Route
+          exact
+          path={`/recipe/:id`}
+          render={routerProps => <Recipe routerProps={routerProps} />}
+        />
+
+        {/* <Route
           path={`${process.env.PUBLIC_URL}/recipe/:id`}
           component={Recipe}
-        />
+        /> */}
       </Switch>
     </BrowserRouter>
   );
