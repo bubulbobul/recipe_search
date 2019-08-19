8b3e57be017b8d645eed65d5b26dc2534323623f
bubulbobul <windam@live.fr>
2019-08-19 16:07:11 +0530

    change recipes

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,49 +1,58 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
 
 function Recipes(props) {
+  console.log("Redcipes props");
+  console.log("Redcipes props", props);
   return (
     <div className='container'>
       <div className='row'>
-        {props.recipes.map(recipe => {
-          return (
-            <div
-              className='col-md-4'
-              key={recipe.recipe_id}
-              style={{ marginBottom: "2rem" }}
-            >
-              <div className='recipes__box'>
-                <img
-                  className='recipes__box-img'
-                  src={recipe.image_url}
-                  alt={recipe.title}
-                />
-                <div className='recipe__text'>
-                  <h5 className='recipes__title'>
-                    {recipe.title.length < 20
-                      ? `${recipe.title}`
-                      : `${recipe.title.substring(0, 24)}...`}
-                  </h5>
-                  <p className='recipes__subtitle'>
-                    Publisher: <span>{recipe.publisher}</span>
-                  </p>
+        {props.recipes === null ? (
+          <div>
+            <h3>Loading...</h3>
+            {console.log("loading")}
+          </div>
+        ) : (
+          <Fragment>
+            {props.recipes.map(recipe => (
+              <div
+                className='col-md-4'
+                key={recipe.recipe_id}
+                style={{ marginBottom: "2rem" }}
+              >
+                <div className='recipes__box'>
+                  <img
+                    className='recipes__box-img'
+                    src={recipe.image_url}
+                    alt={recipe.title}
+                  />
+                  <div className='recipe__text'>
+                    <h5 className='recipes__title'>
+                      {recipe.title.length < 20
+                        ? `${recipe.title}`
+                        : `${recipe.title.substring(0, 24)}...`}
+                    </h5>
+                    <p className='recipes__subtitle'>
+                      Publisher: <span>{recipe.publisher}</span>
+                    </p>
+                  </div>
+                  <button className='recipe_buttons'>
+                    {/* <Link to={`/recipe/${recipe.recipe_id}`}>View Recipe</Link> */}
+                    <Link
+                      to={{
+                        pathname: `/recipe/${recipe.recipe_id}`,
+                        state: { recipe: recipe.title }
+                      }}
+                    >
+                      {" "}
+                      View Recipe
+                    </Link>
+                  </button>
                 </div>
-                <button className='recipe_buttons'>
-                  {/* <Link to={`/recipe/${recipe.recipe_id}`}>View Recipe</Link> */}
-                  <Link
-                    to={{
-                      pathname: `/recipe/${recipe.recipe_id}`,
-                      state: { recipe: recipe.title }
-                    }}
-                  >
-                    {" "}
-                    View Recipe
-                  </Link>
-                </button>
               </div>
-            </div>
-          );
-        })}
+            ))}
+          </Fragment>
+        )}
       </div>
     </div>
   );
