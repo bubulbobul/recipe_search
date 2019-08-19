0f7fb9d55c1c6386592393234499f1d55e6fa8be
bubulbobul <windam@live.fr>
2019-08-19 15:30:25 +0530

    Updates

---
+++ precache-manifest.2348ff9d7d4312dfb0b05dbf0466a236.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f85766691930af476e0c6e4728fd3fa9",
+    "url": "/recipe_search/index.html"
+  },
+  {
+    "revision": "4279e1a9da95bfce91c3",
+    "url": "/recipe_search/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "76f0a6613b645efbefb6",
+    "url": "/recipe_search/static/css/main.0fddf2ae.chunk.css"
+  },
+  {
+    "revision": "4279e1a9da95bfce91c3",
+    "url": "/recipe_search/static/js/2.ff18b18e.chunk.js"
+  },
+  {
+    "revision": "76f0a6613b645efbefb6",
+    "url": "/recipe_search/static/js/main.27baa31b.chunk.js"
+  },
+  {
+    "revision": "34d35dd003493e81b21a",
+    "url": "/recipe_search/static/js/runtime~main.f49f2925.js"
+  }
+]);
